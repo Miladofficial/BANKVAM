8c8df4e68478544286f7bbf2695d140953fb0b52
Miladofficial <63480606+Miladofficial@users.noreply.github.com>
2026-06-06 04:43:17 +0330

    Update VamDatabase.kt

--- app/src/main/java/com/miladofficial/vamtracker/data/database/VamDatabase.kt
+++ app/src/main/java/com/miladofficial/vamtracker/data/database/VamDatabase.kt
@@ -3,5 +3,5 @@ package com.miladofficial.vamtracker.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [], version = 1)
+@Database(entities = [], version = 1, exportSchema = false)
 abstract class VamDatabase : RoomDatabase()
