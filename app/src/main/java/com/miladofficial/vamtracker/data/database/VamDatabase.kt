6534137e221095bf9f809f6b848e494937b5687b
Miladofficial <63480606+Miladofficial@users.noreply.github.com>
2026-06-06 04:35:17 +0330

    Create VamDatabase.kt

---
+++ app/src/main/java/com/miladofficial/vamtracker/data/database/VamDatabase.kt
@@ -0,0 +1,7 @@
+package com.miladofficial.vamtracker.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class VamDatabase : RoomDatabase()
