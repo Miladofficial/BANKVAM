97eff23ca81d5687f5e7ca6fbfc0394f8c9198c1
Miladofficial <63480606+Miladofficial@users.noreply.github.com>
2026-06-07 02:16:40 +0330

    Update VamDatabase.kt

--- app/src/main/java/com/miladofficial/vamtracker/data/database/VamDatabase.kt
+++ app/src/main/java/com/miladofficial/vamtracker/data/database/VamDatabase.kt
@@ -1,7 +1,38 @@
-package com.miladofficial.vamtracker.data.database
+package com.miladofficial.vamtracker.data.local.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.miladofficial.vamtracker.data.local.dao.VamDao
+import com.miladofficial.vamtracker.data.local.entity.VamEntity
+import com.miladofficial.vamtracker.utils.DateConverter
 
-@Database(entities = [], version = 1, exportSchema = false)
-abstract class VamDatabase : RoomDatabase()
+@Database(
+    entities = [VamEntity::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(DateConverter::class)
+abstract class VamDatabase : RoomDatabase() {
+
+    abstract fun vamDao(): VamDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: VamDatabase? = null
+
+        fun getDatabase(context: Context): VamDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    VamDatabase::class.java,
+                    "vam_database"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
